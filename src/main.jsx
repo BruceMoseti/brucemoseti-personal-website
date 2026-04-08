c5990f65cc6ef1d836a08ba04e4eec46eb48f7d6
BruceMoseti <brucemosetiA@gmail.com>
2026-04-08 12:55:57 -0400

    Fix GitHub Pages base path and homepage metadata

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/brucemoseti-personal-website">
       <App />
     </BrowserRouter>
   </StrictMode>,
